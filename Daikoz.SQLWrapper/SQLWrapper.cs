234e9320fc6f42ef2614800151f73e59b392cdb3
daikoz <contact@daikoz.com>
2019-04-24 23:02:26 +0200

    Version 0.1.3

--- Daikoz.SQLWrapper/SQLWrapper.cs
+++ Daikoz.SQLWrapper/SQLWrapper.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Runtime.Serialization.Json;
@@ -33,26 +34,76 @@ namespace Daikoz.SQLWrapper
         {
             _log.LogMessage(MessageImportance.Normal, "SQLWrapper: Start");
 
+            uint configIdx = 1;
             foreach (SQLWrapperConfig config in _config)
             {
+                string cacheDBPath = Path.Combine(System.Environment.CurrentDirectory, "obj", "sqlwrapper" + configIdx + ".db");
+                if (!_isCleanning)
+                    CacheDatabase(cacheDBPath, config.ConnectionStrings, config.SQLWrapperPath);
+                else
+                    File.Delete(cacheDBPath);
+
                 string filePattern = config.FilePattern;
                 if (string.IsNullOrWhiteSpace(filePattern))
                     filePattern = "*.sql";
 
                 if (config.RelativePath == null || config.RelativePath.Length == 0)
-                    Execute("", filePattern, config.Namespace, config.ConnectionStrings, config.SQLWrapperPath);
+                    Execute("", filePattern, config.Namespace, config.ConnectionStrings, config.SQLWrapperPath, cacheDBPath);
                 else
                 {
                     foreach (string relativePath in config.RelativePath)
-                        Execute(relativePath, filePattern, config.Namespace, config.ConnectionStrings, config.SQLWrapperPath);
+                        Execute(relativePath, filePattern, config.Namespace, config.ConnectionStrings, config.SQLWrapperPath, cacheDBPath);
                 }
+
+                ++configIdx;
             }
 
             _log.LogMessage(MessageImportance.Normal, "SQLWrapper: End");
             return true;
         }
 
-        private void Execute(string relativePath, string filePattern, string namespaceName, string[] connectionString, string sqlWrapperPath)
+        private void CacheDatabase(string cacheDBPath, string[] connectionStrings, string sqlWrapperPath)
+        {
+            if (File.Exists(cacheDBPath)) return;
+
+            using (Process sqlwrapperProcess = new Process())
+            {
+                string assemblyDirectory = Path.GetDirectoryName(System.Reflection.Assembly.GetAssembly(typeof(SQLWrapper)).Location);
+                if (!Directory.Exists(Path.Combine(assemblyDirectory, "tools")))
+                {
+                    // nuget
+                    assemblyDirectory = Path.Combine(assemblyDirectory, "..", "..");
+                }
+                if (string.IsNullOrWhiteSpace(sqlWrapperPath))
+                    sqlWrapperPath = Path.Combine(assemblyDirectory, "tools", "SQLWrapper.exe");
+
+                sqlwrapperProcess.StartInfo.WorkingDirectory = assemblyDirectory;
+                sqlwrapperProcess.StartInfo.FileName = sqlWrapperPath;
+                sqlwrapperProcess.StartInfo.UseShellExecute = false;
+                sqlwrapperProcess.StartInfo.CreateNoWindow = true;
+                sqlwrapperProcess.StartInfo.RedirectStandardOutput = true;
+                sqlwrapperProcess.StartInfo.RedirectStandardError = true;
+
+                sqlwrapperProcess.StartInfo.Arguments = "database -t mariadb -c ";
+                foreach (string connectionString in connectionStrings)
+                    sqlwrapperProcess.StartInfo.Arguments += " \"" + connectionString.Replace("\"", "\"\"") + "\" ";
+                sqlwrapperProcess.StartInfo.Arguments += "-o " + cacheDBPath;
+
+                sqlwrapperProcess.Start();
+
+                // Synchronously read the standard output of the spawned process. 
+                StreamReader readerOutput = sqlwrapperProcess.StandardOutput;
+                _log.LogWarning(readerOutput.ReadToEnd());
+
+                StreamReader readerError = sqlwrapperProcess.StandardError;
+                string error = readerError.ReadToEnd();
+                if (!string.IsNullOrWhiteSpace(error))
+                    _log.LogError(error);
+
+            }
+        }
+
+        private void Execute(string relativePath, string filePattern, string namespaceName, string[] connectionString, string sqlWrapperPath, string cacheDBPath)
         {
             string directory = Path.Combine(Directory.GetCurrentDirectory(), relativePath);
             if (!Directory.Exists(directory))
@@ -60,72 +111,83 @@ namespace Daikoz.SQLWrapper
             else
             {
                 _log.LogMessage(MessageImportance.Low, "SQLWrapper: Find in directory " + directory + ": " + filePattern);
-                foreach (string file in Directory.EnumerateFiles(directory, filePattern, SearchOption.AllDirectories))
+                List<string> listDirectories = new List<string>(Directory.EnumerateDirectories(directory, "", SearchOption.AllDirectories));
+                listDirectories.Add(directory);
+                foreach (string subdirectory in listDirectories)
                 {
-                    _log.LogMessage(MessageImportance.Low, "SQLWrapper: " + file);
-                    string outputFile = /* Path.Combine(Path.GetDirectoryName(file), Path.GetFileNameWithoutExtension(file)*/ file + ".cs";
-
-                    if (_isCleanning)
+                    List<string> listFiles = new List<string>(Directory.EnumerateFiles(subdirectory, filePattern, SearchOption.TopDirectoryOnly));
+                    if (listFiles.Count > 0)
                     {
+                        _log.LogMessage(MessageImportance.Low, "SQLWrapper: " + subdirectory);
+                        string outputFile = Path.Combine(subdirectory, Path.GetFileName(subdirectory) + ".cs");
+
                         if (File.Exists(outputFile))
                             File.Delete(outputFile);
-                    }
-                    else
-                    {
-                        string assemblyDirectory = Path.GetDirectoryName(System.Reflection.Assembly.GetAssembly(typeof(SQLWrapper)).Location);
-                        if (!Directory.Exists(Path.Combine(assemblyDirectory, "tools")))
-                        {
-                            // nuget
-                            assemblyDirectory = Path.Combine(assemblyDirectory, "..", "..");
-                        }
-                        if (string.IsNullOrWhiteSpace(sqlWrapperPath))
-                            sqlWrapperPath = Path.Combine(assemblyDirectory, "tools", "SQLWrapper.exe");
 
-                        if (!File.Exists(outputFile) || new FileInfo(outputFile).LastWriteTimeUtc <= new FileInfo(sqlWrapperPath).LastWriteTimeUtc || new FileInfo(outputFile).LastWriteTimeUtc <= new FileInfo(file).LastWriteTimeUtc)
+                        if (!_isCleanning)
                         {
-                            using (Process sqlwrapperProcess = new Process())
+                            string assemblyDirectory = Path.GetDirectoryName(System.Reflection.Assembly.GetAssembly(typeof(SQLWrapper)).Location);
+                            if (!Directory.Exists(Path.Combine(assemblyDirectory, "tools")))
                             {
-                                string newNameSpace = namespaceName;
-                                string[] directoriesName = Path.GetDirectoryName(file).Remove(0, Directory.GetCurrentDirectory().Length).Split('\\');
-                                for (uint idx = 0; idx < directoriesName.Length - 1; ++idx)
-                                    if (!string.IsNullOrWhiteSpace(directoriesName[idx]))
-                                        newNameSpace += '.' + directoriesName[idx];
+                                // nuget
+                                assemblyDirectory = Path.Combine(assemblyDirectory, "..", "..");
+                            }
+                            if (string.IsNullOrWhiteSpace(sqlWrapperPath))
+                                sqlWrapperPath = Path.Combine(assemblyDirectory, "tools", "SQLWrapper.exe");
 
-                                string className = directoriesName.Length >= 1 ? directoriesName[directoriesName.Length - 1] : "SQLWrapper";
+                            bool isFileUpdated = false;
+                            foreach (string file in listFiles)
+                                isFileUpdated &= new FileInfo(outputFile).LastWriteTimeUtc <= new FileInfo(file).LastWriteTimeUtc;
 
-                                sqlwrapperProcess.StartInfo.WorkingDirectory = assemblyDirectory;
-                                sqlwrapperProcess.StartInfo.FileName = sqlWrapperPath;
-                                sqlwrapperProcess.StartInfo.UseShellExecute = false;
-                                sqlwrapperProcess.StartInfo.CreateNoWindow = true;
-                                sqlwrapperProcess.StartInfo.RedirectStandardOutput = true;
-                                sqlwrapperProcess.StartInfo.RedirectStandardError = true;
-                                sqlwrapperProcess.StartInfo.Arguments = " -i " + file + " -o " + outputFile + " -n " + newNameSpace + " -c " + className + " -m " + Path.GetFileNameWithoutExtension(file) + " -x " + Path.Combine("tools", "Template", "charpADO.xslt") + " -d ";
-                                foreach (string connString in connectionString)
-                                    sqlwrapperProcess.StartInfo.Arguments += " \"" + connString.Replace("\"", "\"\"") + "\" ";
-                                sqlwrapperProcess.Start();
+                            if (!File.Exists(outputFile) || new FileInfo(outputFile).LastWriteTimeUtc <= new FileInfo(sqlWrapperPath).LastWriteTimeUtc || isFileUpdated)
+                            {
+                                using (Process sqlwrapperProcess = new Process())
+                                {
+                                    string newNameSpace = namespaceName;
+                                    string[] directoriesName = Path.GetDirectoryName(listFiles[0]).Remove(0, Directory.GetCurrentDirectory().Length).Split('\\');
+                                    for (uint idx = 0; idx < directoriesName.Length - 1; ++idx)
+                                        if (!string.IsNullOrWhiteSpace(directoriesName[idx]))
+                                            newNameSpace += '.' + directoriesName[idx];
 
-                                // Synchronously read the standard output of the spawned process. 
-                                StreamReader readerOutput = sqlwrapperProcess.StandardOutput;
-                                _log.LogWarning(readerOutput.ReadToEnd());
+                                    string className = directoriesName.Length >= 1 ? directoriesName[directoriesName.Length - 1] : "SQLWrapper";
 
-                                StreamReader readerError = sqlwrapperProcess.StandardError;
-                                string totot = readerError.ReadToEnd();
-                                _log.LogError(totot);
+                                    sqlwrapperProcess.StartInfo.WorkingDirectory = assemblyDirectory;
+                                    sqlwrapperProcess.StartInfo.FileName = sqlWrapperPath;
+                                    sqlwrapperProcess.StartInfo.UseShellExecute = false;
+                                    sqlwrapperProcess.StartInfo.CreateNoWindow = true;
+                                    sqlwrapperProcess.StartInfo.RedirectStandardOutput = true;
+                                    sqlwrapperProcess.StartInfo.RedirectStandardError = true;
+
+                                    sqlwrapperProcess.StartInfo.Arguments = "wrapper -i";
+                                    foreach (string file in listFiles)
+                                        sqlwrapperProcess.StartInfo.Arguments += " \"" + file.Replace("\"", "\"\"") + "\" ";
+                                    sqlwrapperProcess.StartInfo.Arguments += " -o " + outputFile + " -p namespace=" + newNameSpace + " classname=" + className + " -x " + Path.Combine("tools", "Template", "charpADO.xslt") + " -d " + cacheDBPath;
+                                    sqlwrapperProcess.Start();
+
+                                    // Synchronously read the standard output of the spawned process. 
+                                    StreamReader readerOutput = sqlwrapperProcess.StandardOutput;
+                                    _log.LogWarning(readerOutput.ReadToEnd());
+
+                                    StreamReader readerError = sqlwrapperProcess.StandardError;
+                                    string error = readerError.ReadToEnd();
+                                    if (!string.IsNullOrWhiteSpace(error))
+                                        _log.LogError(error);
 
 
-                                sqlwrapperProcess.WaitForExit();
+                                    sqlwrapperProcess.WaitForExit();
 
 
-                                //////  <Compile Update="DB\AdsMessage\SelectToSend.sql.cs">
-                                //////<DependentUpon>SelectToSend.sql</DependentUpon>
-                                //////  </Compile>
-                                //var csproj = new Microsoft.Build.Evaluation.Project(@"D:\Jobs\src\Lib\Daikoz.ToutVendre\Daikoz.ToutVendre.csproj", null, null, new ProjectCollection());
-                                //var metadata = new List<KeyValuePair<string, string>>();
-                                //metadata.Add(new KeyValuePair<string, string>("DependentUpon", file));
-                                //csproj.AddItem("Compile", outputFile, metadata);
-                                //csproj.ReevaluateIfNecessary();
-                                //csproj.Save();
-                                //Microsoft.Build.Evaluation.ProjectCollection.GlobalProjectCollection.UnloadAllProjects();
+                                    //////  <Compile Update="DB\AdsMessage\SelectToSend.sql.cs">
+                                    //////<DependentUpon>SelectToSend.sql</DependentUpon>
+                                    //////  </Compile>
+                                    //var csproj = new Microsoft.Build.Evaluation.Project(@"D:\Jobs\src\Lib\Daikoz.ToutVendre\Daikoz.ToutVendre.csproj", null, null, new ProjectCollection());
+                                    //var metadata = new List<KeyValuePair<string, string>>();
+                                    //metadata.Add(new KeyValuePair<string, string>("DependentUpon", file));
+                                    //csproj.AddItem("Compile", outputFile, metadata);
+                                    //csproj.ReevaluateIfNecessary();
+                                    //csproj.Save();
+                                    //Microsoft.Build.Evaluation.ProjectCollection.GlobalProjectCollection.UnloadAllProjects();
+                                }
                             }
                         }
                     }
--- Template/charpADO.xslt
+++ Template/charpADO.xslt
@@ -1,6 +1,8 @@
 ﻿<?xml version="1.0" encoding="utf-8"?>
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:msxsl="urn:schemas-microsoft-com:xslt" exclude-result-prefixes="msxsl">
 
+<xsl:param name="namespace"/>
+<xsl:param name="classname"/>
 <xsl:output method="text" omit-xml-declaration="yes" indent="no"/>
 <xsl:preserve-space elements="*" />
 
@@ -26,8 +28,40 @@
 <xsl:template match="output">            public <xsl:apply-templates select="./type"/>&#160;<xsl:value-of select="name"/> { get; set; }
 </xsl:template>
 <xsl:template match="input">, <xsl:apply-templates select="./type"/>&#160;<xsl:value-of select="name"/></xsl:template>
-<xsl:template match="sqlwrapper/input" mode="dapperparam">
+<xsl:template match="input" mode="dapperparam">
   <xsl:if test="position() != 1">, </xsl:if><xsl:value-of select="name"/>
+</xsl:template>
+
+<xsl:template match="sql">
+<!-- Return only one value -->
+<xsl:if test="count(output) = 1 and config/ismultipleresult = 'false'">
+        public static Task&lt;<xsl:apply-templates select="output/type"/>&gt; <xsl:value-of select="config/method"/>(IDbConnection conn, IDbTransaction transaction<xsl:apply-templates select="input"/>)
+        {
+            return conn.ExecuteScalarAsync&lt;<xsl:apply-templates select="output/type"/>&gt;(@"<xsl:value-of select="config/sql"/>"<xsl:if test="input">, new { <xsl:apply-templates select="input" mode="dapperparam"/> }</xsl:if>, transaction: transaction);
+        }
+</xsl:if>
+  
+<!-- Return multiple columns -->  
+<xsl:if test="count(output) > 1">
+        public class <xsl:value-of select="config/method"/>Result
+        {
+<xsl:apply-templates select="output"/>        }
+
+<!-- Return multiple columns and one row -->
+<xsl:if test="config/ismultipleresult = 'false'">
+        public static Task&lt;<xsl:value-of select="config/method"/>Result&gt; <xsl:value-of select="config/method"/>(IDbConnection conn, IDbTransaction transaction<xsl:apply-templates select="input"/>)
+        {
+            return conn.QuerySingleOrDefaultAsync&lt;<xsl:value-of select="config/method"/>Result&gt;(@"<xsl:value-of select="config/sql"/>"<xsl:if test="input">, new { <xsl:apply-templates select="input" mode="dapperparam"/> }</xsl:if>, transaction: transaction);
+        }
+</xsl:if>
+  <!-- Return multiple columns and multiple rows -->
+<xsl:if test="config/ismultipleresult = 'true'">
+        public static Task&lt;IEnumerable&lt;<xsl:value-of select="config/method"/>Result&gt;&gt; <xsl:value-of select="config/method"/>(IDbConnection conn, IDbTransaction transaction<xsl:apply-templates select="input"/>)
+        {
+            return conn.QueryAsync&lt;<xsl:value-of select="config/method"/>Result&gt;(@"<xsl:value-of select="config/sql"/>"<xsl:if test="input">, new { <xsl:apply-templates select="input" mode="dapperparam"/> }</xsl:if>, transaction: transaction);
+        }
+</xsl:if>
+</xsl:if>
 </xsl:template>
 
 <xsl:template match="/">using Dapper;
@@ -36,39 +70,11 @@ using System.Data;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace <xsl:value-of select="sqlwrapper/config/namespace"/>
+namespace <xsl:value-of select="$namespace"/>
 {
-    public partial class <xsl:value-of select="sqlwrapper/config/class"/>
+    public partial class <xsl:value-of select="$classname"/>
     {
-<!-- Return only one value -->
-<xsl:if test="count(sqlwrapper/output) = 1 and sqlwrapper/config/ismultipleresult = 'false'">
-        public static Task&lt;<xsl:apply-templates select="sqlwrapper/output/type"/>&gt; <xsl:value-of select="sqlwrapper/config/method"/>(IDbConnection conn, IDbTransaction transaction<xsl:apply-templates select="sqlwrapper/input"/>)
-        {
-            return conn.ExecuteScalarAsync&lt;<xsl:apply-templates select="sqlwrapper/output/type"/>&gt;(@"<xsl:value-of select="sqlwrapper/config/sql"/>"<xsl:if test="sqlwrapper/input">, new { <xsl:apply-templates select="sqlwrapper/input" mode="dapperparam"/> }</xsl:if>, transaction: transaction);
-        }
-</xsl:if>
-  
-<!-- Return multiple columns -->  
-<xsl:if test="count(sqlwrapper/output) > 1">
-        public class <xsl:value-of select="sqlwrapper/config/method"/>Result
-        {
-<xsl:apply-templates select="sqlwrapper/output"/>        }
-
-<!-- Return multiple columns and one row -->
-<xsl:if test="sqlwrapper/config/ismultipleresult = 'false'">
-        public static Task&lt;<xsl:value-of select="sqlwrapper/config/method"/>Result&gt; <xsl:value-of select="sqlwrapper/config/method"/>(IDbConnection conn, IDbTransaction transaction<xsl:apply-templates select="sqlwrapper/input"/>)
-        {
-            return conn.QuerySingleOrDefaultAsync&lt;<xsl:value-of select="sqlwrapper/config/method"/>Result&gt;(@"<xsl:value-of select="sqlwrapper/config/sql"/>"<xsl:if test="sqlwrapper/input">, new { <xsl:apply-templates select="sqlwrapper/input" mode="dapperparam"/> }</xsl:if>, transaction: transaction);
-        }
-</xsl:if>
-  <!-- Return multiple columns and multiple rows -->
-<xsl:if test="sqlwrapper/config/ismultipleresult = 'true'">
-        public static Task&lt;IEnumerable&lt;<xsl:value-of select="sqlwrapper/config/method"/>Result&gt;&gt; <xsl:value-of select="sqlwrapper/config/method"/>(IDbConnection conn, IDbTransaction transaction<xsl:apply-templates select="sqlwrapper/input"/>)
-        {
-            return conn.QueryAsync&lt;<xsl:value-of select="sqlwrapper/config/method"/>Result&gt;(@"<xsl:value-of select="sqlwrapper/config/sql"/>"<xsl:if test="sqlwrapper/input">, new { <xsl:apply-templates select="sqlwrapper/input" mode="dapperparam"/> }</xsl:if>, transaction: transaction);
-        }
-</xsl:if>
-</xsl:if>
+<xsl:apply-templates select="sqlwrapper/sql"/>
     }
   
 } 
